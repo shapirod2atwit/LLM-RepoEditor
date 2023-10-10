8696ddcc6f27a655c24540c1b387aa9a0742c0d6
shapirod2atwit <shapirod2@wit.edu>
2023-10-10 13:47:58 -0400

    create ASTs for js files

--- src/test.js
+++ src/test.js
@@ -1,28 +1,43 @@
 const Parser = require('tree-sitter');
 const JavaScript = require('tree-sitter-javascript');
+const fs = require('fs');
+const path = require('path');
 
 const parser = new Parser();
 parser.setLanguage(JavaScript);
 
-//change to take files
-const sourceCode = 'let x = 1; console.log(x);';
-const tree = parser.parse(sourceCode);
+//arr to store all abstract syntax trees
+var ASTs = [];
 
-console.log(tree.rootNode.toString());
+//put the path to the repository here
+const target = "./src";
 
-// (program
-//   (lexical_declaration
-//     (variable_declarator (identifier) (number)))
-//   (expression_statement
-//     (call_expression
-//       (member_expression (identifier) (property_identifier))
-//       (arguments (identifier)))))
+// Loop through all the files in the directory
+fs.readdir(target, async function (err, files) {
+  if (err) {
+    console.error("Could not list the directory.", err);
+    process.exit(1);
+  }
 
-const callExpression = tree.rootNode.child(1).firstChild;
-console.log(callExpression);
+  files.forEach(function (file, index) {
+    //make complete path of file
+    var currentPath = path.join(target, file);
+    var type = path.extname(currentPath);
 
-// { type: 'call_expression',
-//   startPosition: {row: 0, column: 16},
-//   endPosition: {row: 0, column: 30}, 
-//   startIndex: 0,
-//   endIndex: 30 }
+    fs.stat(currentPath, function (error, stat) {
+      if (error) {
+        console.error("Error stating file.", error);
+        return;
+      }
+
+      if (stat.isFile() && type==".js"){
+        //conver file to string
+        var sourceCode = fs.readFileSync(currentPath).toString();
+        //create ast and push ast array
+        ASTs.push(parser.parse(sourceCode));
+        console.log("'%s' AST has been added to AST list.", currentPath);
+        
+      }
+    });
+  });
+});
