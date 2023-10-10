a303616c749a3d1ad3d38d8c3110a828881115b6
shapirod2atwit <shapirod2@wit.edu>
2023-10-10 14:24:54 -0400

    can now recursively scan repository

--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 const Parser = require('tree-sitter');
+//add more language parsers later
 const JavaScript = require('tree-sitter-javascript');
 const fs = require('fs');
 const path = require('path');
@@ -12,32 +13,43 @@ var ASTs = [];
 //put the path to the repository here
 const target = "./src";
 
+createASTs(target);
+
+function createASTs(folder){
 // Loop through all the files in the directory
-fs.readdir(target, async function (err, files) {
-  if (err) {
-    console.error("Could not list the directory.", err);
-    process.exit(1);
-  }
-
-  files.forEach(function (file, index) {
-    //make complete path of file
-    var currentPath = path.join(target, file);
-    var type = path.extname(currentPath);
-
-    fs.stat(currentPath, function (error, stat) {
-      if (error) {
-        console.error("Error stating file.", error);
-        return;
-      }
-
-      if (stat.isFile() && type==".js"){
-        //conver file to string
-        var sourceCode = fs.readFileSync(currentPath).toString();
-        //create ast and push ast array
-        ASTs.push(parser.parse(sourceCode));
-        console.log("'%s' AST has been added to AST list.", currentPath);
+    fs.readdir(folder, async function (err, files) {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            process.exit(1);
+        }
+    
+        files.forEach(function (file, index) {
+            //make complete path of file
+            var currentPath = path.join(folder, file);
+            //get file extension type
+            var type = path.extname(currentPath);
         
-      }
+            //check is the file is a file or directory
+            fs.stat(currentPath, function (error, stat) {
+                if (error) {
+                    console.error("Error stating file.", error);
+                    return;
+                }
+        
+                //only check for js files right now
+                if (stat.isFile() && type==".js"){
+                    //convert file to string
+                    var sourceCode = fs.readFileSync(currentPath).toString();
+                    //create ast and push ast array
+                    ASTs.push(parser.parse(sourceCode));
+                    console.log("'%s' AST has been added to AST list.", currentPath);
+                    console.log(ASTs.pop().rootNode.toString());
+                
+                }else if(stat.isDirectory()){
+                    //recursive call to find any files in folders
+                    createASTs(currentPath);
+                }
+            });
+        });
     });
-  });
-});
+}
