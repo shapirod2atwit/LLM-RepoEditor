32a7c9e9f9683d9c581daff5ebcfd5d9aa65212b
shapirod2atwit <shapirod2@wit.edu>
2023-10-19 15:49:00 -0400

    file clean up/ dependency graph update

--- src/Main.cjs
+++ src/Main.cjs
@@ -7,7 +7,7 @@ const parser = new Parser();
 parser.setLanguage(JavaScript);
 
 // Define the directory to analyze
-const rootDirectory = './src'; // Adjust this to your repository path
+const rootDirectory = './TestFiles'; // Adjust this to your repository path
 
 // Define the nodes and edges for the dependency graph
 const dependencyGraph = new Map();
@@ -30,42 +30,45 @@ const relations = {
   InstantiatedBy: 'InstantiatedBy',
   Uses: 'Uses',
   UsedBy: 'UsedBy',
+  Invokes: 'Invokes',
+  InvokedBy: 'InvokedBy',
 };
 
 // Function to parse JavaScript files
-function parseJavaScriptFile(filePath) {
+function parseFile(filePath) {
   const code = fs.readFileSync(filePath, 'utf-8');
   const tree = parser.parse(code);
-  const dependencies = [];
+  const dependencies = {
+    [relations.Imports]: [],
+    [relations.BaseClassOf]: [],
+    [relations.Calls]: [],
+    [relations.Invokes]: [],
+    [relations.Uses]: [],
+  };
 
   // Search for import statements in the abstract syntax tree (AST)
   tree.rootNode.descendantsOfType('import_statement').forEach((node) => {
-    const importPath = node.text.replace(/['"]/g, '');
-    dependencies.push(importPath);
+    dependencies[relations.Imports].push(node.text);
   });
 
   // Search for inherited classes in the abstract syntax tree (AST)
   tree.rootNode.descendantsOfType('class_heritage').forEach((node) => {
-    const importPath = node.text.replace(/['"]/g, '');
-    dependencies.push(importPath);
+    dependencies[relations.BaseClassOf].push(node.text);
   });
 
   // Search for method calls in the abstract syntax tree (AST)
   tree.rootNode.descendantsOfType('call_expression').forEach((node) => {
-    const importPath = node.text.replace(/['"]/g, '');
-    dependencies.push(importPath);
+    dependencies[relations.Calls].push(node.text);
   });
 
   // Search for method invocations in the abstract syntax tree (AST)
   tree.rootNode.descendantsOfType('invocation_expression').forEach((node) => {
-    const importPath = node.text.replace(/['"]/g, '');
-    dependencies.push(importPath);
+    dependencies[relations.Invokes].push(node.text);
   });
 
   // Search for field use relations in the abstract syntax tree (AST)
   tree.rootNode.descendantsOfType('element_access_expression').forEach((node) => {
-    const importPath = node.text.replace(/['"]/g, '');
-    dependencies.push(importPath);
+    dependencies[relations.Uses].push(node.text);
   });
 
   //class_heritage, call_expression, invocation_expression, element_access_expression
@@ -84,10 +87,10 @@ function buildDependencyGraph(directory) {
     if (stats.isDirectory()) {
       // If it's a directory, recursively analyze its contents
       buildDependencyGraph(filePath);
-    } else if (stats.isFile() && file.endsWith('.js')) {
-      // If it's a JavaScript file, analyze it
-      const dependencies = parseJavaScriptFile(filePath);
-      dependencyGraph.set(filePath, { [relations.Imports]: dependencies });
+    } else if (stats.isFile() && file.endsWith('.cs')) {
+      // If it's a C# file, analyze it
+      const dependencies = parseFile(filePath);
+      dependencyGraph.set(filePath, dependencies);
     }
   });
 }
@@ -99,7 +102,11 @@ function main() {
   // Print the dependency graph
   dependencyGraph.forEach((value, key) => {
     console.log(`File: ${key}`);
-    console.log(`Dependencies: ${value[relations.Imports].join(', ')}`);
+    console.log(`Imports: ${value[relations.Imports].join(', ')}`);
+    console.log(`BaseClass: ${value[relations.BaseClassOf].join(', ')}`);
+    console.log(`Calls: ${value[relations.Calls].join(', ')}`);
+    console.log(`Invokes: ${value[relations.Invokes].join(', ')}`);
+    console.log(`Uses: ${value[relations.Uses].join(', ')}`);
   });
 }
 
